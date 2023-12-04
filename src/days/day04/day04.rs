2e60059f2ff238363c6acf99a02e4123f01678eb
David Price <dpricedev@gmail.com>
2023-12-04 12:27:39 +0000

    day 4 part 2

--- src/days/day04/day04.rs
+++ src/days/day04/day04.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use crate::utils::file as file;
 
 use regex::Regex;
@@ -15,29 +15,43 @@ pub fn run() {
 
 fn part_one() -> u32 {
     file::get_lines("input/day04/input.txt").iter()
-        .map(get_wins)
+        .map(get_win_count)
+        .map(|count| if count > 0 { 2_u32.pow((count - 1) as u32) } else { 0 })
         .sum::<u32>()
 }
 
 fn part_two() -> u32 {
-    let input = file::get_lines("input/day04/example.txt");
+    let counts = file::get_lines("input/day04/input.txt").iter()
+        .map(get_win_count)
+        .collect::<Vec<usize>>();
 
+    let mut card_counts: HashMap<usize, u32> = HashMap::new();
+    for (index, count) in counts.iter().enumerate() {
+        let additional_count = update_card_count(&mut card_counts, index, 1);
 
+        for offset in 0..*count {
+            update_card_count(&mut card_counts, index + offset + 1, additional_count);
+        }
+    }
 
-    0
+    card_counts.values().sum::<u32>()
 }
 
-fn get_wins(input: &String) -> u32 {
+fn update_card_count(counts: &mut HashMap<usize, u32>, index: usize, additional_count: u32) -> u32 {
+    let new_count = counts.get(&index).unwrap_or(&0) + additional_count;
+    counts.insert(index, new_count);
+    return new_count
+}
+
+fn get_win_count(input: &String) -> usize {
     static INPUT_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"([\d ]+)").unwrap());
 
     let (_, [winning_numbers]) = INPUT_REGEX.captures_iter(&input).nth(1).unwrap().extract();
     let (_, [our_numbers]) = INPUT_REGEX.captures_iter(&input).nth(2).unwrap().extract();
 
-    let intersect_count = get_number_set(winning_numbers)
+    get_number_set(winning_numbers)
         .intersection(&get_number_set(our_numbers))
-        .count();
-
-    if intersect_count > 0 { 2_u32.pow((intersect_count - 1) as u32) } else { 0 }
+        .count()
 }
 
 fn get_number_set(input: &str) -> HashSet<u32> {
