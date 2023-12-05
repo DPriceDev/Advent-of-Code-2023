c1e786473a1405adbed5e182097cf12ca71bd42a
David Price <dpricedev@gmail.com>
2023-12-05 23:34:32 +0000

    added some more functional methods

--- src/days/day05/day05.rs
+++ src/days/day05/day05.rs
@@ -1,5 +1,4 @@
-use std::cmp::{max, min, Ordering};
-use std::collections::{HashMap, HashSet};
+use std::cmp::{min};
 use crate::utils::file as file;
 
 use regex::Regex;
@@ -14,11 +13,6 @@ pub fn run() {
     println!("  part 2: {} in {}ms", part_two(), now.elapsed().as_millis());
 }
 
-struct Product {
-    next: String,
-    ranges: Vec<Range>,
-}
-
 struct Mapping {
     start: usize,
     dest_start: usize,
@@ -30,82 +24,29 @@ struct Range {
     end: usize,
 }
 
-
-
 fn part_one() -> usize {
     let almanac = file::get_lines("input/day05/input.txt");
 
-    let mut seeds_part = get_seeds(almanac.clone().into_iter().nth(0).unwrap().as_str())
-        .into_iter()
-        .map(|seeds| Range { start: seeds, end: seeds })
+    let seeds = get_seeds(almanac.iter().nth(0).unwrap().as_str())
+        .iter()
+        .map(|seeds| Range { start: *seeds, end: *seeds })
         .collect::<Vec<Range>>();
 
-    let mut row_index = 2;
-    while row_index < almanac.len() {
-        let (mappings, new_index) = get_mappings(row_index, &almanac);
-        seeds_part = map_ranges(&seeds_part, &mappings);
-        row_index = new_index
-    }
-
-    seeds_part.into_iter()
-        .min_by(|a, b| a.start.cmp(&b.start))
-        .unwrap()
-        .start
+    get_lowest_seed(&almanac, seeds)
 }
 
 fn part_two() -> usize {
     let almanac = file::get_lines("input/day05/input.txt");
 
-    let seeds = get_seeds(almanac.clone().into_iter().nth(0).unwrap().as_str());
-
-    let mut seeds_part = seeds.into_iter()
-        .as_slice()
+    let seeds = get_seeds(almanac.iter().nth(0).unwrap().as_str())
         .chunks(2)
         .map(|seeds| {
-            Range {
-                start: *seeds.into_iter().nth(0).unwrap(),
-                end: *seeds.into_iter().nth(0).unwrap() + *seeds.into_iter().nth(1).unwrap()
-            }
+            let start = *seeds.iter().nth(0).unwrap();
+            Range { start, end: start + *seeds.iter().nth(1).unwrap() }
         })
         .collect::<Vec<Range>>();
 
-    let mut row_index = 2;
-    while row_index < almanac.len() {
-        let (mappings, new_index) = get_mappings(row_index, &almanac);
-        seeds_part = map_ranges(&seeds_part, &mappings);
-        row_index = new_index
-    }
-
-    seeds_part.into_iter()
-        .min_by(|a, b| a.start.cmp(&b.start))
-        .unwrap()
-        .start
-
-    // let input = Vec::from([Range { start: 0, end: 12 }]);
-    //
-    // let mappings = Vec::from([
-    //     Mapping {
-    //         start: 2,
-    //         dest_start: 40,
-    //         range: 5,
-    //     },
-    //     Mapping {
-    //         start: 10,
-    //         dest_start: 50,
-    //         range: 7,
-    //     },
-    //     Mapping {
-    //         start: 50,
-    //         dest_start: 100,
-    //         range: 7,
-    //     },
-    // ]);
-    //
-    // let new_ranges = map_ranges(&input, &mappings);
-    //
-    // for new_range in new_ranges {
-    //     println!("{} - {}", new_range.start, new_range.end)
-    // }
+    get_lowest_seed(&almanac, seeds)
 }
 
 fn get_seeds(input: &str) -> Vec<usize> {
@@ -115,37 +56,32 @@ fn get_seeds(input: &str) -> Vec<usize> {
         .collect::<Vec<usize>>()
 }
 
-fn get_mappings(index: usize, almanac: &Vec<String>) -> (Vec<Mapping>, usize) {
-    let mut row_index = index + 1;
-
-    let mut list = Vec::new();
-    // row_index += 1;
-    static MAPPING_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"\d+").unwrap());
-    while row_index < almanac.len() {
-        let row = almanac.into_iter().nth(row_index).unwrap().as_str();
-
-        if row.is_empty() {
-            row_index += 1;
-            return (list, row_index)
-        }
-
-        let destination_start = MAPPING_REGEX.find_iter(row).nth(0).unwrap().as_str().parse::<usize>().unwrap();
-        let source_start = MAPPING_REGEX.find_iter(row).nth(1).unwrap().as_str().parse::<usize>().unwrap();
-        let range = MAPPING_REGEX.find_iter(row).nth(2).unwrap().as_str().parse::<usize>().unwrap();
-
-        list.push(Mapping {
-            start: source_start,
-            dest_start: destination_start,
-            range,
-        });
-
-        row_index += 1;
-    }
-
-    (list, row_index)
+fn get_lowest_seed(almanac: &Vec<String>, seeds: Vec<Range>) -> usize {
+    get_mappings(&almanac).iter()
+        .fold(seeds, |running, mapping| map_ranges(&running, &mapping))
+        .iter()
+        .min_by(|a, b| a.start.cmp(&b.start))
+        .unwrap()
+        .start
 }
 
-// generic function that takes input ranges and maps them into output ranges
+fn get_mappings(almanac: &Vec<String>) -> Vec<Vec<Mapping>> {
+    static MAPPING_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"\d+").unwrap());
+    almanac.split(|row| row.is_empty() || row.chars().any(|c| c.is_alphabetic()))
+        .filter(|group| !group.is_empty())
+        .map(Vec::from)
+        .map(|mapping_group| {
+            mapping_group.iter().map(|entry| {
+                Mapping {
+                    start: MAPPING_REGEX.find_iter(&*entry).nth(1).unwrap().as_str().parse::<usize>().unwrap(),
+                    dest_start: MAPPING_REGEX.find_iter(&*entry).nth(0).unwrap().as_str().parse::<usize>().unwrap(),
+                    range: MAPPING_REGEX.find_iter(&*entry).nth(2).unwrap().as_str().parse::<usize>().unwrap(),
+                }
+            }).collect::<Vec<Mapping>>()
+        })
+        .collect::<Vec<Vec<Mapping>>>()
+}
+
 fn map_ranges(input_ranges: &Vec<Range>, mappings: &Vec<Mapping>) -> Vec<Range> {
     input_ranges.iter()
         .map(|input_range| map_range(input_range, &mappings))
@@ -157,14 +93,11 @@ fn map_range(input_range: &Range, sorted_mappings: &Vec<Mapping>) -> Vec<Range> 
     let mut new_ranges = Vec::new();
 
     let mut start = input_range.start;
-    let mappings_in_range = sorted_mappings.into_iter()
+    let mappings_in_range = sorted_mappings.iter()
         .filter(|mapping| mapping_contains_range(input_range, mapping))
         .collect::<Vec<&Mapping>>();
 
     for mapping in &mappings_in_range {
-       println!("filtered mappings = {} - {}", mapping.start, mapping.range);
-       println!("input range = {} - {}", start, input_range.end);
-
         if start < mapping.start {
             new_ranges.push(Range { start, end: mapping.start - 1 });
             start = mapping.start;
@@ -176,7 +109,7 @@ fn map_range(input_range: &Range, sorted_mappings: &Vec<Mapping>) -> Vec<Range> 
 
             new_ranges.push(Range {
                 start: mapping.dest_start + range_start,
-                end: mapping.dest_start + range_end
+                end: mapping.dest_start + range_end,
             });
 
             start = mapping.start + mapping.range
